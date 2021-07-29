319db551b01ec1350e90a3ef07a23408156c7bc0
Yefim Savin <savin.efim2@gmail.com>
2021-07-29 23:49:03 +0500

    add forgotten classes

---
+++ src/main/java/com/crud/config/WebAppInitialiser.java
@@ -0,0 +1,17 @@
+package com.crud.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitialiser extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{Config.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
